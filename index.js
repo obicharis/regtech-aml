b105f3ac41630b191e5179a140123e19d28ec52a
obicharis <charisobinna4@gmail.com>
2026-06-20 13:17:11 +0100

    Serve static files from the 'public' directory

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const riskLedgerRoutes = require('./routes/riskLedger');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.json({ status: 'RegTech AML API running' });
